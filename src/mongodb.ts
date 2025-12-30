c08922bc12743ee78216247b007ef1f506c6fbf1
Pieter Soudan <pietersoudan@gmail.com>
2025-12-30 13:41:35 +0100

    support different distros for deps treeshaking

---
+++ src/mongodb.ts
@@ -0,0 +1,6 @@
+// MongoDB distribution entry point
+// Re-exports core functionality plus MongoDB store
+
+export * from './index'; // Core functionality
+export { MongoEventStore, MongoEventStoreOptions } from './eventstore/stores/mongodb';
+
---
+++ src/postgres.ts
@@ -0,0 +1,6 @@
+// Postgres distribution entry point
+// Re-exports core functionality plus Postgres store
+
+export * from './index'; // Core functionality
+export { PostgresEventStore, PostgresEventStoreOptions } from './eventstore/stores/postgres';
+
