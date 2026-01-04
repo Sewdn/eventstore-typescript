8c4280a27cd2dda7f04a381c375302a9633a0c26
Pieter Soudan <pietersoudan@gmail.com>
2026-01-04 13:19:23 +0100

    Import MemoryEventStreamNotifier directly to avoid loading notifiers index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@
 // Exports: types, filters, notifiers, and memory store only
 
 export { MemoryEventStore } from './eventstore/stores/memory';
-export { MemoryEventStreamNotifier } from './eventstore/notifiers';
+export { MemoryEventStreamNotifier } from './eventstore/notifiers/memory';
 export { createFilter, createQuery } from './eventstore/filter';
 export * from './eventstore/types';
